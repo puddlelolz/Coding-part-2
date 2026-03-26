bf87f845ae9967cf22696489ff642be6b3fad106
puddlelolz <fadhli.ildaf722@gmail.com>
2026-03-26 16:57:52 +0700

    commit

---
+++ 2. GUI/mood tracker.py
@@ -0,0 +1,15 @@
+import tkinter as fn #It's a library
+#find what library is
+
+#1. setup the windows
+root = fn.Tk()
+root.title("mood tracker by puddle")
+root.geometry("500x500")
+
+
+
+
+
+
+
+root.mainloop()

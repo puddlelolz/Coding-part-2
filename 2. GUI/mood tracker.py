ad6b7b1afa82dc6cdf0acef9fa7b0cb182c4a517
puddlelolz <fadhli.ildaf722@gmail.com>
2026-03-28 21:56:01 +0800

    commit

--- 2. GUI/mood tracker.py
+++ 2. GUI/mood tracker.py
@@ -1,15 +1,23 @@
 import tkinter as fn #It's a library
-#find what library is
+#library is toolbox for programmers, it is a pre made code that we can use, so we doesnt need to write in from zero
 
 #1. setup the windows
 root = fn.Tk()
 root.title("mood tracker by puddle")
 root.geometry("500x500")
 
+#2. creating functions for buttons
+def smile():
+    label = fn.Label(root, text="keep smiling 😁😁", fg="yellow")
+    label.pack()
 
 
+def unmotivated():
+    label = fn.Label(root, text="search motivation online dude dont ask me", fg="gray")
+    label.pack()
 
-
-
+def cant_do_nothing():
+    label = fn.Label(root, text="THATS WHY YOU SHOULD NOT ON YOUR PHONE ALL DAY", fg="red")
+    label.pack()
 
 root.mainloop()

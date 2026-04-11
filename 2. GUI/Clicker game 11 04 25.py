7a2985b3d81f6c074f0ee30e7a6ced3fcfef0aee
puddlelolz <fadhli.ildaf722@gmail.com>
2026-04-11 16:57:17 +0800

    commit

---
+++ 2. GUI/Clicker game 11 04 25.py
@@ -0,0 +1,11 @@
+import tkinter as fn
+
+#1. variables
+score = 0
+points_per_click = 1
+upgrade_cost = 10
+
+#2. setup the windows
+root = fn.Tk()
+root.title("Clicker Game by Puddle")
+root.geometry("400x500")

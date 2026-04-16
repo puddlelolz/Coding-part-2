e547ff48bfba5c9e87a5331ecb5bdac915b5b169
puddlelolz <fadhli.ildaf722@gmail.com>
2026-04-16 17:56:10 +0800

    commit

--- 2. GUI/Clicker game 11 04 25.py
+++ 2. GUI/Clicker game 11 04 25.py
@@ -9,3 +9,16 @@ upgrade_cost = 10
 root = fn.Tk()
 root.title("Clicker Game by Puddle")
 root.geometry("400x500")
+
+#3. creating label and fuctions
+score_label = fn.Label(root, text=f"Score: {score}", font=("Arial", 24))
+score_label.pack(pady=20)
+
+def click():
+    global score #using this code will make th program understand which variable do i need to change
+    score += points_per_click
+    score_label.config(text=f"Score: {score}")
+
+
+
+root.mainloop()

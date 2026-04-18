d008f4827930153ab8af60e541e5786135e0f9f5
puddlelolz <fadhli.ildaf722@gmail.com>
2026-04-18 16:43:19 +0800

    commit

--- 2. GUI/Clicker game 11 04 25.py
+++ 2. GUI/Clicker game 11 04 25.py
@@ -1,4 +1,5 @@
 import tkinter as fn
+from tkinter import messagebox as nf
 
 #1. variables
 score = 0
@@ -19,6 +20,22 @@ def click():
     score += points_per_click
     score_label.config(text=f"Score: {score}")
 
+def clicking():
+    global score, points_per_click, upgrade_cost
+    if score >= upgrade_cost:
+        score -= upgrade_cost
+        points_per_click += 1
+        upgrade_cost *= 2
+        score_label.config(text=f"Score: {score}")
+        upgrade_button.config(text=f"Upgrade (Cost: {upgrade_cost})")
+    else:
+        nf.showinfo("you are poor", "click more man you are broke")
 
+#4. creating buttons
+click_button = fn.Button(root, text="smash me", command=click)
+click_button.pack(pady=20)
+
+upgrade_button = fn.Button(root, text=f"Upgrade (Cost: {upgrade_cost})", command=clicking)
+upgrade_button.pack(pady=20)
 
 root.mainloop()

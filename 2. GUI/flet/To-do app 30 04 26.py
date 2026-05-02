3250d246295d9142924fc1814d6d7ec568a85325
puddlelolz <fadhli.ildaf722@gmail.com>
2026-05-02 17:00:29 +0800

    Commit

--- 2. GUI/flet/To-do app 30 04 26.py
+++ 2. GUI/flet/To-do app 30 04 26.py
@@ -6,5 +6,19 @@ def pt(page: fn.Page):
     page.window.width = 1234
     page.window.height = 1000
     page.theme_mode =fn.ThemeMode.DARK
-    
+
+
+    box = fn.TextField(
+        label="what do you plan to do today?",
+        value="input what to do here",
+        border=fn.InputBorder.OUTLINE#hellooooo
+    )
+    button = fn.ElevatedButton(
+        "add to list",
+        bgcolor=fn.Colors.ORANGE_800,
+        color = fn.Colors.RED_900
+    )
+
+    page.add(box, button)
+    page.update()
 fn.app(target=pt)
